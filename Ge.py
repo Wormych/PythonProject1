b5938e4bde190f7a9cbe71f4e890fbfbb8e5cbe0
illya <illyarakov2009@gmail.com>
2025-04-10 17:48:20 +0300

    hmtryexcept

--- Ge.py
+++ Ge.py
@@ -1,6 +1,39 @@
 import telebot
-
+import random
+from telebot import types
+import datetime
+import sqlite3
 bot = telebot.TeleBot("7396973751:AAF4ThS5i6Kh-73VsxKibAKA2w34qORohks")
+
+data = datetime.datetime.now()
+
+
+
+
+
+
+
+@bot.message_handler(commands=["start"])
+def welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("🎲 Яку оцінку сьогодні отримаєш?")
+    item2 = types.KeyboardButton("😊 Як справи?")
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, "Hi!".format(message.from_user, bot.get_me()),parse_mode='html', reply_markup=markup)
+
+def save_message_to_file(user_id, message_text):
+    with open("user_messages.txt", "a", encoding="utf-8") as file:
+        file.write(f"User {user_id}: {message_text}\n")
+        connection = sqlite3.connect("itsteps_DB.sl3", 5)
+        cur = connection.cursor()
+        # cur.execute("CREATE TABLE MessageBot (data TEXT,message TEXT)")
+        cur.execute("INSERT INTO MessageBot (data) VALUES (?)", (data,))
+        cur.execute("INSERT INTO MessageBot (message) VALUES (?)", (message_text,))
+        connection.commit()
+        res = cur.fetchall()
+        print(res)
+        connection.commit()
+        connection.close()
 
 @bot.message_handler(commands=["meme"])
 def welcome(message):
@@ -44,4 +77,49 @@ def audio(message):
 def welcome(message):
     bot.send_message(message.chat.id, "/music,/film,/book,/sport,/friends")
 
+@bot.message_handler(content_types=['text'])
+def processing_text(message):
+    save_message_to_file(message.from_user.id, message.text)
+    if message.text == "🎲 Яку оцінку сьогодні отримаєш?":
+        bot.send_message(message.chat.id, random.randint(2,12))
+    elif message.text  == "😊 Як справи?":
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        item1 = types.InlineKeyboardButton("Нормально", callback_data='good')
+        item2 = types.InlineKeyboardButton("Плохо", callback_data='bad')
+        item3 = types.InlineKeyboardButton("Возможно", callback_data='4t')
+        markup.add(item1, item2, item3)
+        bot.send_message(message.chat.id, "Чудово! Помив підлогу?".format(message.from_user, bot.get_me()),parse_mode='html', reply_markup=markup)
+
+    elif message.text  == "Привіт":
+        bot.send_message(message.chat.id, "Здоров")
+    elif message.text == "чіназес":
+        bot.send_message(message.chat.id, "саунтрес")
+    else:
+        bot.send_message(message.chat.id, "Я не зрозумів тебе")
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                bot.send_message(call.message.chat.id, 'От і добре ')
+                bot.answer_callback_query(callback_query_id=call.id, show_alert=True,
+                    text="Cool!!!!!")
+            elif call.data == 'bad':
+                bot.send_message(call.message.chat.id, 'Your nigodyai')
+                bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+                    text="Run!!!!")
+
+            elif call.data == '4t':
+                bot.send_message(call.message.chat.id, 'Clear floor')
+                bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+                    text="Okay")
+
+            # remove inline buttons
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                  text="😊 Як справи?", reply_markup=None)
+
+            # show alert
+    except Exception as e:
+        print(repr(e))
 bot.polling()
